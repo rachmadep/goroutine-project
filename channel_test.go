905bcea5c988616eed48f74b40ccebc61657ba53
Rachmad Pribadi <rachmad@jakpat.net>
2025-04-07 14:18:00 +0700

    Channel In and Out

--- channel_test.go
+++ channel_test.go
@@ -40,3 +40,24 @@ func TestChannelAsParameter(t *testing.T) {
 
 	time.Sleep(3 * time.Second) // Wait for the goroutine to finish
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(1 * time.Second) // Simulate some work
+	channel <- "Hello, Channel!"
+}
+
+func OnlyOut(channel <-chan string) {
+	message := <- channel
+	fmt.Println("Received message:", message)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	// Wait for the goroutines to finish
+	time.Sleep(3 * time.Second)
+}
