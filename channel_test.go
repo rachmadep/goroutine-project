d7d2b91fecb1d3a26944b7e2fae5750f0247ae46
Rachmad Pribadi <rachmad@jakpat.net>
2025-04-07 14:11:00 +0700

    channel as parameter

--- channel_test.go
+++ channel_test.go
@@ -22,3 +22,21 @@ func TestCreateChannel(t *testing.T) {
 
 	time.Sleep(2 * time.Second) // Wait for the goroutine to finish
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(1 * time.Second) // Simulate some work
+	channel <- "Hello, Channel!"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	// Receive the message from the channel
+	message := <- channel
+	fmt.Println("Received message:", message)
+
+	time.Sleep(3 * time.Second) // Wait for the goroutine to finish
+}
