0d7974cbfb3db73678c52b15d5ec47dd6a6c80c5
Rachmad Pribadi <rachmad@jakpat.net>
2025-04-09 10:03:11 +0700

    Buffered Channel

--- channel_test.go
+++ channel_test.go
@@ -61,3 +61,30 @@ func TestInOutChannel(t *testing.T) {
 	// Wait for the goroutines to finish
 	time.Sleep(3 * time.Second)
 }
+
+// Buffered channels
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Hello, Channel 1!"
+		channel <- "Hello, Channel 2!"
+		// channel <- "Hello, Channel 3!"
+	}()
+
+	go func ()  {
+		time.Sleep(1 * time.Second) // Simulate some work
+		fmt.Println("Buffered channel size:", len(channel))
+		fmt.Println("Buffered channel capacity:", cap(channel))
+
+		fmt.Println("Received message:", <- channel)
+		fmt.Println("Received message:", <- channel)
+		// fmt.Println("Received message:", <- channel)
+	}()
+
+	time.Sleep(2 * time.Second) // Wait for the goroutines to finish
+
+	fmt.Println("Done with buffered channel")
+}
