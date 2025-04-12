993161c90b1f0e8c1683d0d3ad12d36d9ce96bbc
Rachmad Pribadi <rachmad@jakpat.net>
2025-04-12 11:23:45 +0700

    Select Channel

--- channel_test.go
+++ channel_test.go
@@ -108,3 +108,32 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("Done with range channel")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+			case data := <- channel1:
+				fmt.Println("Received message from channel1:", data)
+				counter++
+			case data := <- channel2:
+				fmt.Println("Received message from channel2:", data)
+				counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+	
+
+	fmt.Println("Done with select channel")
+}
