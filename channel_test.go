d3ca7823634c05ec6de809686c282682014dcf2e
Rachmad Pribadi <rachmad@jakpat.net>
2025-04-12 11:28:03 +0700

    Default Select Channel

--- channel_test.go
+++ channel_test.go
@@ -137,3 +137,34 @@ func TestSelectChannel(t *testing.T) {
 
 	fmt.Println("Done with select channel")
 }
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+			case data := <- channel1:
+				fmt.Println("Received message from channel1:", data)
+				counter++
+			case data := <- channel2:
+				fmt.Println("Received message from channel2:", data)
+				counter++
+			default:
+				fmt.Println("No messages received, doing other work...")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+	
+
+	fmt.Println("Done with default select channel")
+}
