6395d7f41c4ed3a1c7bcd61e9d9eef06b2726779
Rachmad Pribadi <rachmad@jakpat.net>
2025-04-18 14:44:41 +0700

    Simulate Deadlock

--- mutex_test.go
+++ mutex_test.go
@@ -69,3 +69,51 @@ func TestRWMutex(t *testing.T) {
 
 	fmt.Println("Final balance:", account.GetBalance())
 }
+
+type UserBalance struct {
+	Mutex sync.Mutex
+	Name  string
+	Balance int
+}
+
+func (user *UserBalance) Lock() {
+	user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+
+func (user *UserBalance) Change(amount int) {
+	user.Balance += amount
+}
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock()
+	fmt.Println("Locking user1:", user1.Name)
+	user1.Change(-amount)
+
+	time.Sleep(1 * time.Second)
+	
+	user2.Lock()
+	fmt.Println("Locking user2:", user2.Name)
+	user2.Change(amount)
+
+	time.Sleep(1 * time.Second)
+
+	user1.Unlock()
+	user2.Unlock()
+}
+
+func TestDeadlock(t *testing.T) {
+	user1 := &UserBalance{Name: "User1", Balance: 10000}
+	user2 := &UserBalance{Name: "User2", Balance: 10000}
+
+	go Transfer(user1, user2, 100)
+	go Transfer(user2, user1, 100)
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("Final balance of user1:", user1.Balance)
+	fmt.Println("Final balance of user2:", user2.Balance)
+}
