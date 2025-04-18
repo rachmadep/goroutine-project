da0b3e17d70e389a5404ef93fb2fcf90552d0164
Rachmad Pribadi <rachmad@jakpat.net>
2025-04-18 14:17:02 +0700

    RWMutex

--- mutex_test.go
+++ mutex_test.go
@@ -33,3 +33,39 @@ func TestMutex(t *testing.T) {
 	// Check the value of x
 	fmt.Println("Final value of x:", x)
 }
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	defer account.RWMutex.Unlock()
+
+	account.Balance += amount
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	defer account.RWMutex.RUnlock()
+
+	return account.Balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 1; i <= 100; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(4 * time.Second)
+
+	fmt.Println("Final balance:", account.GetBalance())
+}
